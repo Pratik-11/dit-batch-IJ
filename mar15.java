5cb86fadb801e11c6e1ce2b6c9a10b4626ea8e21
Pratik S <pratiksingh.sskr@gmail.com>
2024-03-15 11:43:21 +0530

    15mar

--- mar15.java
+++ mar15.java
@@ -16,9 +16,49 @@ public class mar15 {
         res = res*n;
         factV(n-1, res);
     }
+
+    static void printTable(int n, int res){
+        if(res == 0){
+            return;
+        }
+        printTable(n, res-1);
+        System.out.println(n + " x " + res + " = " + n*res);
+    }
+
+    static String printTableString(int n,int res){
+        if(res == 0){
+            return " ";
+        }
+        String result =  printTableString(n, res-1);
+        String s = (n + " x " + res + " = " + n*res + "\n") ;
+        result = result + s;
+        // System.out.println(s);
+        return result;
+    }
+
+    static void PlallinDrom(int n,int reverse, int orig){
+        if( n == 0){
+            System.out.println(reverse == orig);
+            return;
+        }
+        reverse = (reverse*10 + n%10);
+        PlallinDrom(n/10,reverse,orig);
+    }
+
+    static boolean PlallinDromB(int n,int reverse, int orig){
+        if( n == 0){
+            return (reverse == orig);
+        }
+        reverse = (reverse*10 + n%10);
+        return PlallinDromB(n/10,reverse,orig);
+    }
  
     public static void main(String[] args) {
-        System.out.println(fact(5));
-        factV(5, 1);
+        // System.out.println(fact(5));
+        // factV(5, 1);
+        // printTable(5, 10);
+        System.out.println(printTableString(5, 10));
+        PlallinDrom(101,0,101);
+        System.out.println(PlallinDromB(101,0,101));
     }
 }
