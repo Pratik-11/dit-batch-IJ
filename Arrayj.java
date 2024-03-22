b637769e61c7cabcca6af9b9076f3ca53350b646
Pratik S <pratiksingh.sskr@gmail.com>
2024-03-22 11:14:51 +0530

    15mar

---
+++ Arrayj.java
@@ -0,0 +1,32 @@
+public class Arrayj {
+
+    static int[] arrOccurenceHelper(int[] arr,int index, int count,int element){
+
+        
+        if(index == arr.length){
+            int[] arr1 = new int[count];
+            return arr1;
+        }
+        if(arr[index] == element){
+            count++;
+        }
+        int[] arr1 = arrOccurenceHelper(arr, index+1, count, element);
+        if(arr[index] == element){
+            arr1[count-1] = index;
+            count--;
+        }
+        return arr1;
+
+    }
+
+    static int[] arrOccurence(int[] arr,int index,int element){
+        return arrOccurenceHelper(arr, index, 0, element);
+    }
+    public static void main(String[] args) {
+        int[] arr = { 1,2,3,2,3,2,3,2,2,2};
+        for(int i : arrOccurence(arr, 0, 2)){
+            System.out.println(i);
+        }
+  
+    }
+}
