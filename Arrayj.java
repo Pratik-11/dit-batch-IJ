0373a8bc3ea3745d76b0932ca9ba54df2c90a7e9
Pratik S <pratiksingh.sskr@gmail.com>
2024-03-22 11:33:14 +0530

    22mar

--- Arrayj.java
+++ Arrayj.java
@@ -1,8 +1,6 @@
 public class Arrayj {
 
-    static int[] arrOccurenceHelper(int[] arr,int index, int count,int element){
-
-        
+    static int[] arrOccurenceHelper(int[] arr,int index, int count,int element){   
         if(index == arr.length){
             int[] arr1 = new int[count];
             return arr1;
@@ -16,17 +14,29 @@ public class Arrayj {
             count--;
         }
         return arr1;
+    }
 
+    // static String revStringHelper(String str,int count){
+
+    // }
+
+    static String revString(String str){
+        if(str == ""){
+            return " ";
+        }
+        String r = revString(str.substring(1));
+        r = r + str.charAt(0);
+        return r;
     }
 
     static int[] arrOccurence(int[] arr,int index,int element){
         return arrOccurenceHelper(arr, index, 0, element);
     }
     public static void main(String[] args) {
-        int[] arr = { 1,2,3,2,3,2,3,2,2,2};
-        for(int i : arrOccurence(arr, 0, 2)){
-            System.out.println(i);
-        }
-  
+        // int[] arr = { 1,2,3,2,3,2,3,2,2,2};
+        // for(int i : arrOccurence(arr, 0, 2)){
+        //     System.out.println(i);
+        // }
+        System.out.println(revString("abcd"));
     }
 }
